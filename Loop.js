d783ea94e0272f4da0b4e0b1c0d84b2a7537c25f
agraham <agraham0925@gmail.com>
2018-03-13 17:31:45 -0500

    answered question 2

--- Loop.js
+++ Loop.js
@@ -5,3 +5,13 @@ for(let i=0; i <= 20; i++) {
 			console.log(i + " is odd")
 		}
 }
+
+// 2.
+for(let i=1; i <= 100; i++) {
+	if(i % 3){
+		console.log("Fizz")} else if(i % 5){
+			console.log("Buzz")	} else if(i % 3 && i & 5){
+				console.log("FizzBuzz")} else {
+					console.log(i)
+				}
+}
